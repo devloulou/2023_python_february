0da94653c70766aa9c2ae9a3369c5627c912a5b8
Richard Kovacs <dev.kovacs.richard90@gmail.com>
2023-05-08 22:44:30 +0200

    22. alkalom

---
+++ 22. alkalom/stack.py
@@ -0,0 +1,46 @@
+"""
+Docker
+Git
+
+Docker: konténerizációs technológia
+
+Monolith rendszerek - 1 óriási, összefüggő rendszer
+Microservicek alapú fejlesztés - kis komponensek, amelyek "önállóan" is tudnak működni
+
+Virtuális Gépek - Virtual Machine - VM
+
+Docker - használni tudjunk olyan szoftvereket, applikációkat, amelyeket fel kellene telepíteni,
+        de, a Docker lehetőséget ad arra, hogy Dockerben futtassuk ezeket
+
+        - fejlesztői környezet kialakítása: template - szöveges paraméterfile-ok
+        - microservicek fejlesztése
+
+"""
+
+"""
+Docker telepítése után:
+    - CLI - command line interface
+    - Docker Host - Docker Deamon
+
+Docker használat lépései:
+
+opcionális az első lépés
+
+Dockerfile  -> Docker fileből build -elsz egy image-et -> az image run (fusson, mint egy container)
+
+"""
+
+
+"""
+Docker parancsok
+
+imagek listázása:
+docker image ls
+
+containerek listázása
+docker container ls
+
+
+
+
+"""
